cff86af7d3365b1f1c3caaf6e22eafed757a5d3d
as10896 <as10896@gmail.com>
2018-08-13 08:49:02 +0800

    Fixed the bug of init the weight of pretrained embedding

--- models/encoder.py
+++ models/encoder.py
@@ -20,7 +20,7 @@ class Encoder(nn.Module):
 
         if fixed_embeddings:
             self.embedding.weight.requires_grad = False
-        
+
         self.word_vec_size = self.embedding.embedding_dim
 
         self.rnn_type = rnn_type
@@ -28,19 +28,19 @@ class Encoder(nn.Module):
                            input_size=self.word_vec_size,
                            hidden_size=self.hidden_size,
                            num_layers=self.num_layers,
-                           dropout=self.dropout, 
+                           dropout=self.dropout,
                            bidirectional=self.bidirectional)
-        
+
         self.init_parameters()
 
-        
+
 
     def forward(self, src_seqs):
         emb = self.embedding(src_seqs)              # (max_seq_len, batch_size) -> (max_seq_len, batch_size, emb_dim)
         batch_size = src_seqs.size(1)
         hidden = self.initHidden(batch_size)        # (num_layers * num_directions, batch_size, hidden_size)
         outputs, hidden = self.rnn(emb, hidden)     # (max_seq_len, batch, hidden_size), (num_layers * num_directions, batch_size, hidden_size)
-        
+
         if self.bidirectional:
             hidden = self._cat_directions(hidden)
         return outputs, hidden                      # (max_seq_len, batch, hidden_size), (num_layers, batch_size, num_directions * hidden_size)
@@ -48,16 +48,16 @@ class Encoder(nn.Module):
     def _cat_directions(self, hidden):
         def _cat(h):
             return torch.cat([h[0:h.size(0):2], h[1:h.size(0):2]], 2)
-        
+
         if isinstance(hidden, tuple):
             # LSTM hidden contains a tuple (hidden state, cell state)
             hidden = tuple([_cat(h) for h in hidden])
         else:
             # GRU hidden
             hidden = _cat(hidden)
-            
+
         return hidden
-    
+
     def initHidden(self, batch_size):
         if self.rnn_type == "LSTM":
             return (torch.zeros(self.num_layers * self.num_directions, batch_size, self.hidden_size, device=device),
@@ -66,7 +66,8 @@ class Encoder(nn.Module):
             return torch.zeros(self.num_layers * self.num_directions, batch_size, self.hidden_size, device=device)
 
     def init_parameters(self):
-        for param in self.parameters():
+        for name, param in self.named_parameters():
+            if name.startswith("embedding"): continue
             if param.requires_grad == True:
                 if param.ndimension() >= 2:
                     nn.init.xavier_normal_(param)
@@ -83,20 +84,20 @@ from opts import LOAD_CHECKPOINT, opts, USE_CUDA, USE_PARALLEL
 if LOAD_CHECKPOINT:
     from opts import checkpoint
     from dataset.gen_dataset import gen_dataset
-    encoder = Encoder(embedding=nn.Embedding(len(gen_dataset.vocab.token2id), opts.word_vec_size, padding_idx=PAD), 
-                        hidden_size=opts.hidden_size, 
-                        num_layers=opts.num_layers, 
-                        dropout=opts.dropout, 
-                        bidirectional=opts.bidirectional, 
+    encoder = Encoder(embedding=nn.Embedding(len(gen_dataset.vocab.token2id), opts.word_vec_size, padding_idx=PAD),
+                        hidden_size=opts.hidden_size,
+                        num_layers=opts.num_layers,
+                        dropout=opts.dropout,
+                        bidirectional=opts.bidirectional,
                         fixed_embeddings=opts.fixed_embeddings)
     encoder.load_state_dict(checkpoint['encoder_state_dict'])
 else:
     from embedding.load_emb import embedding
-    encoder = Encoder(embedding=embedding, 
-                        hidden_size=opts.hidden_size, 
-                        num_layers=opts.num_layers, 
-                        dropout=opts.dropout, 
-                        bidirectional=opts.bidirectional, 
+    encoder = Encoder(embedding=embedding,
+                        hidden_size=opts.hidden_size,
+                        num_layers=opts.num_layers,
+                        dropout=opts.dropout,
+                        bidirectional=opts.bidirectional,
                         fixed_embeddings=opts.fixed_embeddings)
 
 if USE_CUDA:
